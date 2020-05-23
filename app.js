33b52ebf8f1baa9595db1b8592bc56ef2de13924
P4yin <60865374+P4yin@users.noreply.github.com>
2020-05-23 11:39:47 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js"); //my nama jeff
 const bot = new Discord.Client();
 const Scrambo = require("scrambo");
 const cube = new Scrambo();
